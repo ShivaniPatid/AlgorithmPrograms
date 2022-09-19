e11638474d8a77cd6245b247d54fda8ef20766f8
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-19 17:26:04 +0530

    [Shivani] 1. All permutations of a String

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -10,6 +10,22 @@ namespace AlgorithmPrograms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose a option");
+            Console.WriteLine("1. All permutations of a String");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    string str = "ABC";
+                    int n = str.Length;
+                    StringPermutations.Permute(str, 0, n - 1);
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
